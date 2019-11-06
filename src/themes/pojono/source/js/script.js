d6cd8f1696cde69c70caa9834ece777b5afecf33
pojono <pojono@gmail.com>
2019-11-06 08:12:36 -0800

    fix script

--- src/themes/pojono/source/js/script.js
+++ src/themes/pojono/source/js/script.js
@@ -1,41 +1,13 @@
-// declaraction of document.ready() function.
-/*
-(function () {
-    var ie = !!(window.attachEvent && !window.opera);
-    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
-    var fn = [];
-    var run = function () {
-        for (var i = 0; i < fn.length; i++) fn[i]();
-    };
-    var d = document;
-    d.ready = function (f) {
-        if (!ie && !wk && d.addEventListener)
-            return d.addEventListener('DOMContentLoaded', f, false);
-        if (fn.push(f) > 1) return;
-        if (ie)
-            (function () {
-                try {
-                    d.documentElement.doScroll('left');
-                    run();
-                } catch (err) {
-                    setTimeout(arguments.callee, 0);
-                }
-            })();
-        else if (wk)
-            var t = setInterval(function () {
-                if (/^(loaded|complete)$/.test(d.readyState))
-                    clearInterval(t), run();
-            }, 0);
-    };
-})();
-*/
-
 document.addEventListener("DOMContentLoaded",
     // toggleTheme function.
     // this script shouldn't be changed.
     function () {
         console.log("DOM LOADED!");
-        var _Blog = window._Blog || {};
+        let _Blog = window._Blog || {};
+        const defaultTheme = "dark";
+        if (!window.localStorage.getItem('theme')) {
+            window.localStorage.setItem('theme', defaultTheme);
+        }
         const currentTheme = window.localStorage && window.localStorage.getItem('theme');
         const isDark = currentTheme === 'dark';
         if (isDark) {
@@ -84,7 +56,5 @@ document.addEventListener("DOMContentLoaded",
             })
         };
         _Blog.toggleTheme();
-
-        // ready function.
     }
 );
