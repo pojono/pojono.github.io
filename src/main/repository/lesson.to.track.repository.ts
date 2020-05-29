309762db08a3b60f6fe1044f085903dc7b759fac
Ivan Chulkov <ivan.chulkov@citronium.com>
2020-05-30 01:41:20 +0300

    fix orderIndex for tracks in lesson and rubrics

--- src/main/repository/lesson.to.track.repository.ts
+++ src/main/repository/lesson.to.track.repository.ts
@@ -4,6 +4,13 @@ import { LessonToTrack } from '../entity/lesson.to.track.entity';
 @EntityRepository(LessonToTrack)
 export class LessonToTrackRepository extends Repository<LessonToTrack> {
   async findByLessonId(lessonId: number): Promise<LessonToTrack[]> {
-    return LessonToTrack.find({ where: { lessonId } });
+    return LessonToTrack.find({
+      where: {
+        lessonId,
+      },
+      order: {
+        orderIndex: 'ASC',
+      },
+    });
   }
 }
