4fd47699250d841927f14e570ae785f8713ea0cd
Ivan Chulkov <ivan.chulkov@citronium.com>
2020-06-16 11:38:39 +0300

    add info on the main screen; fix strikes;

--- src/main/response/dto/main.stats.response.ts
+++ src/main/response/dto/main.stats.response.ts
@@ -9,4 +9,13 @@ export class MainStatsResponseDto {
 
   @ApiModelProperty({ type: 'number', nullable: false })
   public readonly maxStrike: number;
+
+  @ApiModelProperty({ type: 'number', nullable: false })
+  public readonly myCurrentStrike: number;
+
+  @ApiModelProperty({ type: 'number', nullable: false })
+  public readonly myMaxStrike: number;
+
+  @ApiModelProperty({ type: 'number', nullable: false })
+  public readonly myTodayTime: number;
 }
