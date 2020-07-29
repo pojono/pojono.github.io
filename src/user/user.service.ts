fd1af307a05a996ff109a3a989b9d45d35712749
Ivan Chulkov <ivan.chulkov@citronium.com>
2020-07-29 12:51:53 +0300

    fix back

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -406,11 +406,11 @@ export class UserService {
   async updateStrike(user: User, utcDiff: number): Promise<void> {
     const todayDate = moment
       .utc()
-      .add(utcDiff, 'minutes')
+      .add(utcDiff * -1, 'minutes')
       .startOf('day');
     const lastActivityDate = moment
       .utc(user.lastActivity)
-      .add(utcDiff, 'minutes')
+      .add(utcDiff * -1, 'minutes')
       .startOf('day');
     const strikeDiff = todayDate.diff(lastActivityDate, 'days');
 
