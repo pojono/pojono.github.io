9b8dabddbb8ab895d17d580d2f8405c78e49cf10
Ivan Chulkov <ivan.chulkov@citronium.com>
2020-09-22 00:21:42 +0300

    check subscriptionEndDate

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -144,8 +144,13 @@ export class UserRepository extends Repository<User> {
     user.subscriptionLatestReceipt = subscriptionDto.latestReceipt;
     user.subscriptionValidationResponse = subscriptionDto.validationResponse;
     user.subscriptionStartDate = subscriptionDto.startDate;
-    user.subscriptionEndDate = subscriptionDto.endDate;
     user.subscriptionIsCancelled = subscriptionDto.isCancelled;
+
+    const curEndDate = moment.utc(user.subscriptionEndDate);
+    const newEndDate = moment.utc(subscriptionDto.endDate);
+    if (!curEndDate.isValid() || newEndDate.isAfter(curEndDate)) {
+      user.subscriptionEndDate = subscriptionDto.endDate;
+    }
 
     return user.save();
   }
