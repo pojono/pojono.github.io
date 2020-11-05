0ef51eff351a7e836cfc53053293354ab7b44892
pojono <pojono@gmail.com>
2020-11-05 17:58:53 +0300

    show news after the any lesson

--- src/main/service/event.service.ts
+++ src/main/service/event.service.ts
@@ -114,21 +114,10 @@ export class EventService {
         const finishedLessons: number = await this.statisticLessonService.countFinishedByUserId(
           user.id,
         );
-        if (finishedLessons === 1 && !user.pushesTime) {
-          const quiz = await this.quizService.getByEventDescription(
-            EventDescriptionEnum.GO_TO_PUSHES,
-          );
-          if (quiz) {
-            return quiz.id;
-          }
-        } else if (
-          finishedLessons > 1 &&
+        if (
+          finishedLessons > 0 &&
           latestNewsQuiz &&
           user.latestNewsQuizId !== latestNewsQuiz.id
-          /*
-          !user.ratingQuizFinished &&
-          isTrue(eventRequestDto.appHasRating)
-          */
         ) {
           await this.userService.newsQuizFinished(user, latestNewsQuiz.id);
           return latestNewsQuiz.id;
