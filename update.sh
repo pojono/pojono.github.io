5dc82533ddc34c71f94e8af02dd77513b4376207
pojono <pojono@gmail.com>
2020-12-24 22:15:35 +0300

    refactor for pages

--- update.sh
+++
@@ -1,4 +0,0 @@
-docker build -t blog . && \
-docker tag blog:latest pojono/blog:latest && \
-docker push pojono/blog:latest && \
-cd ansible && ansible-playbook -vv -u ubuntu playbook.yml -i hosts --tags "blog" && cd ..
